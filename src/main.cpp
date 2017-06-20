70d1a275034d025fc3acf27032b64be2a3f131e2
luukvansloun <luukvansloun@live.nl>
2017-06-20 13:51:06 +0200

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,29 @@
 
 #include "Parser/Parser.h"
 #include <iostream>
-
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char** argv){
 
     Parser parser;
-
-
-
-    if (string(argv[1]) == "1") {
-        parser.XmlParser("");
-        System* system = parser.getSystem();
-        system->properlyparsed();
-        system->Output();
-    } else if (string(argv[1]) == "2") {
-        parser.XmlParser("testInput/MultipleErrors.xml");
-        System* system = parser.getSystem();
-        system->properlyparsed();
+    System* system;
+    if(argc > 1){
+        if(!strcmp(argv[1],"1")){
+            parser.XmlParser(argv[2]);
+        }
+        else if(!strcmp(argv[1],"2")) {
+            parser.XmlParser("testInput/MultipleErrors.xml");
+        }
     }
     else{
         parser.XmlParser("InputFiles/XML_Test.xml");
-        System* system = parser.getSystem();
-        system->properlyparsed();
-        system->autoSimulation();
-        system->Output();
+
+
     }
+    system = parser.getSystem();
+    system->Output();
+    system->properlyparsed();
+    system->autoSimulation();
 }
